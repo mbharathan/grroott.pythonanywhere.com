412f744b9b360f4ea6207ecb15cf27be1dbe54be
Gokul nath <gokulnath634@gmail.com>
2020-06-16 13:34:13 +0530

    Creating UpdateView to make the user to edit the post

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
 
 	
@@ -29,6 +29,14 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 		form.instance.author = self.request.user
 		return super().form_valid(form)
 
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+	model = Post
+	fields = ['title', 'content']
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
 		
 
 def about(request):
