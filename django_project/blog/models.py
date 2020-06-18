ffa061ba353a277eb52f92b94d52a92e1f9dda12
Gokul nath <gokulnath634@gmail.com>
2020-06-18 13:03:19 +0530

    Added see post & set min char length while posting content

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.urls import reverse
+from django.core.validators import MinLengthValidator
 
 class Post(models.Model):
 	title = models.CharField(max_length=100)
-	content = models.TextField()
+	content = models.TextField(validators=[MinLengthValidator(200)])
 	date_posted = models.DateTimeField(default=timezone.now)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
 
